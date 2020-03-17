8f85a43cead6ff493729488800d8255ea8790d1c
angelicatiara <55955834+angelicatiara@users.noreply.github.com>
2020-03-17 16:00:06 +0700

    Create generator.js

---
+++ generator.js
@@ -0,0 +1,7 @@
+const newQuoteButton = document.querySelector('.new-quote');
+
+function getQuote() {
+	console.log("newQuoteButton was clicked");
+}
+
+newQuoteButton.addEventListener('click', getQuote);
