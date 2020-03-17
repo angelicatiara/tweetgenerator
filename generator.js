760e8743b3f504ab64897aa61738906426f1a8e5
angelicatiara <55955834+angelicatiara@users.noreply.github.com>
2020-03-17 17:10:48 +0700

    Update generator.js
    Added function displayQuote to display on website

--- generator.js
+++ generator.js
@@ -1,19 +1,24 @@
-const newQuoteButton = document.querySelector('.new-quote');
+const endpoint = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
 
-const endpoint = "https://api.whatdoestrumpthink.com/api/v1/quotes/random"
 function getQuote() {
 	fetch(endpoint)
 	.then(function (response) {
 		return response.json();
 	})
 	.then(function (data) {
-		console.log(data);
+		displayQuote(data.message);
 	})
 	.catch(function () {
 		console.log("Oops. It's not you. It's us :(");
 	});
 }
 
+function displayQuote(quote) {
+	const quoteText = document.querySelector('.quote-text');
+	quoteText.textContent = quote;
+}
+
+ const newQuoteButton = document.querySelector('.new-quote');
 newQuoteButton.addEventListener('click', getQuote);
 
 
