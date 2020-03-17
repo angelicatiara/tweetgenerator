bb28cc80f68cb7348559f2dddcedc1d5c27f1275
angelicatiara <55955834+angelicatiara@users.noreply.github.com>
2020-03-17 16:56:35 +0700

    Update generator.js
    Add API endpoint for Trump's Quote generator

--- generator.js
+++ generator.js
@@ -1,7 +1,19 @@
 const newQuoteButton = document.querySelector('.new-quote');
 
+const endpoint = "https://api.whatdoestrumpthink.com/api/v1/quotes/random"
 function getQuote() {
-	console.log("newQuoteButton was clicked");
+	fetch(endpoint)
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (data) {
+		console.log(data);
+	})
+	.catch(function () {
+		console.log("Oops. It's not you. It's us :(");
+	});
 }
 
 newQuoteButton.addEventListener('click', getQuote);
+
+
